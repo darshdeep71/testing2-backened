2a8669a495f7d966563abd0c362210e29f196266
darshdeep71 <darshdeep86@gmail.com>
2025-04-24 23:32:35 +0530

    first trying to run backened on render

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // atlas
 
-// let mongoose = require("mongoose");
+let mongoose = require("mongoose");
 
-// let atlass = 'mongodb+srv://test-yt:test123@test.yr3cp.mongodb.net/?retryWrites=true&w=majority&appName=test';
+let atlass = 'mongodb+srv://test-yt:test123@test.yr3cp.mongodb.net/?retryWrites=true&w=majority&appName=test';
 
-// mongoose.connect(atlass).then( ()=>{
-//     console.log("connected")
-// } ).catch( ()=>{
-//     console.log("111")
-// } );
+mongoose.connect(atlass).then( ()=>{
+    console.log("connected")
+} ).catch( ()=>{
+    console.log("111")
+} );
 
 // atlas
 
@@ -16,7 +16,7 @@
 
 // compass
 
-let mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/live');
+// let mongoose = require("mongoose");
+// mongoose.connect('mongodb://localhost:27017/live');
 
 // compass
